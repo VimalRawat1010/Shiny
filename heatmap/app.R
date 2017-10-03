2aac17ce53a6702261f71b67148f77e0a8273c72
Vimal <vimal.biochem@gmail.com>
2017-10-03 07:39:00 +0200

    Circos done git add .!

--- heatmap/app.R
+++ heatmap/app.R
@@ -19,7 +19,7 @@ library(RCurl)
 library(d3Network)
 library(networkD3)
 
-
+library(OmicCircos);
 
 
 
@@ -29,7 +29,7 @@ ui <- navbarPage(
                   inverse = TRUE,
                   tabPanel(HTML(paste0("<b> <h4> <a href=", shQuote("https://github.com/VimalRawat1010"), ">", "&nbsp;Github", "</a> </h3></b>"))),
                   tabPanel(HTML(paste0("<h3><b>&nbsp;&nbsp;&nbsp;&nbsp;Arabidopsis Gene Expression Landscape</b><h1>"))),
-                  fluidPage(theme ="bootstrap.css",
+                  fluidPage(theme ="darkly.css",
                             # Load d3.js
                             tags$head(
                               tags$script(src = 'http://d3js.org/d3.v3.min.js')
@@ -40,7 +40,7 @@ ui <- navbarPage(
                             inverse = FALSE,
                         sidebarLayout(
                            sidebarPanel(
-                               textAreaInput("TxA1", "Type ATG ids here: ",rows=5, width='100%', resize = "both", value="AT1G10220\nAT1G27280\nAT1G01400\n"),
+                               textAreaInput("TxA1", "Type ATG ids here: ",rows=5, width='100%', resize = "both", value="AT1G10220\nAT1G27280\nAT1G01400\nAT1G01120\nAT1G01110\nAT1G01130\n"),
                                fileInput("file", "or Upload the file with ATG ids"),
                                h3(actionButton("enterdata", "Enter", style='font-size:100%'))
 
@@ -57,16 +57,18 @@ ui <- navbarPage(
                                                   ),
                                                   tabPanel(h4("Gene list"),
                                                            #HTML("<h4><b>How to use this webtool to plot your data.</b></h4>"),
+                                                           selectInput("tableGene", "Choose a Source:", c("MA"= "MA", "RNAseq" = "RNAseq"), selected=NULL),
                                                             h4(textOutput("geneExp")),
                                                             h4(tableOutput("data_sets"))
                                                             ),
                                                   tabPanel(h4("Expression profile"),
                                                             #HTML("<h4><b>How to use this webtool to plot your data.</b></h4>"),
+                                                           selectInput("tableExp", "Choose a Source:", c("MA"= "MA", "RNAseq" = "RNAseq"), selected=NULL),
                                                            checkboxInput("Bx_cb_row", "Show dendogram for row", value = TRUE, width = NULL),
                                                            checkboxInput("Bx_cb_col", "Show dendogram for column", value = TRUE, width = NULL),
                                                            radioButtons("Rb_col", "Select Color pallete", c("Red-Green" = "redgreen","Heat" = "heat.colors","Terrain" = "terrain.colors","Topo" = "topo.colors","CM" = "cm.colors"), inline = TRUE),
                                                            #tags$style(HTML(".radio-inline {margin-right: 80px;}")),
-                                                           h4(uiOutput("tissue")),
+                                                           h5(uiOutput("tissue")),
                                                            sliderInput("cex1","Row Label size", min=0.1, max=2, value=1),
                                                            sliderInput("cex2","Col Label size", min=0.1, max=2, value=1),
                                                            h5(plotOutput("heatmap"))
@@ -76,7 +78,16 @@ ui <- navbarPage(
                                                            sliderInput('slider', label = 'Choose node opacity',min = 0, max = 1, step = 0.01, value = 0.5),
                                                            htmlOutput('networkPlot')
                                                             
-                                                            )
+                                                            ),
+                                                 tabPanel(h4("Genomic Location"),
+                                                          HTML("<h4><b>How to use this webtool to plot your data.</b></h4>"),
+                                                          h5(plotOutput("locationCircos"))
+                                                          #htmlOutput('networkPlot')
+                                                          
+                                                 )
+                                                 
+                                                 
+                                                 
                                                     ),
                                     verbatimTextOutput("TxA1")
                                               )
@@ -129,7 +140,7 @@ server <- function(input, output,session) {
           on.exit(dbDisconnect(conn1), add = TRUE)
           geneList  <- gsub("\n", "','", input$TxA1)
           
-          rs= dbGetQuery(conn1,paste0("SELECT  *  FROM RNAseq WHERE Gene IN ('" , geneList , "');"))
+          rs= dbGetQuery(conn1,paste0("SELECT  *  FROM ", input$tableGene , " WHERE Gene IN ('" , geneList , "');"))
           return (rs)
         })
       })
@@ -156,30 +167,86 @@ server <- function(input, output,session) {
                 geneList  <- gsub("\n", "','", input$TxA1)
                 #tissueList  <- gsub("\n", "','", as.symbol(input$tissue))
                 #print(geneList)
-                rs= dbSendQuery(conn,paste0("SELECT  *  FROM RNAseq WHERE Gene IN ('" , geneList , "');"))
+                rs= dbSendQuery(conn,paste0("SELECT  *  FROM ", input$tableExp , " WHERE Gene IN ('" , geneList , "');"))
                 heatmap.df = fetch(rs, n=-1)
        return (heatmap.df)
 
   }
 
-
- 
-
-  getData<-reactive({
-          data <- getHeatmap()
-          data <- data[,input$Tissue]
-          return(data)
+  
+  output$heatmap <- renderPlot({
+    data <- getData()
+    #print(data)
+    row.names(data) <- data$Gene
+    data <- data[-c(1)]
+    #print(data)
+    heatmap.2(as.matrix(data), trace="none", scale="row",col= input$Rb_col, cexRow = input$cex1, cexCol = input$cex2, Rowv = input$Bx_cb_row, Colv = input$Bx_cb_col)
   })
   
   
-  output$heatmap <- renderPlot({
-                    data <- getData()
-                    #print(data)
-                    row.names(data) <- data$Gene
-                    data <- data[-c(1)]
-                    #print(data)
-                    heatmap.2(as.matrix(data), trace="none", scale="row",col= input$Rb_col, cexRow = input$cex1, cexCol = input$cex2, Rowv = input$Bx_cb_row, Colv = input$Bx_cb_col)
-                })
+#### This function gives sexpression values for a subset of tissue   
+  getData<-reactive({
+    data <- getHeatmap()
+    data <- data[,input$Tissue]
+    return(data)
+  })
+  
+  
+  
+  getLocation <- function(){
+    
+      conn <- dbConnect(
+      drv = RMySQL::MySQL(),
+      dbname = "Athal_Expression",
+      host = "localhost",
+      username = "root",
+      password = "abc123")
+    on.exit(dbDisconnect(conn), add = TRUE)
+    geneList  <- gsub("\n", "','", input$TxA1)
+    #tissueList  <- gsub("\n", "','", as.symbol(input$tissue))
+    #print(geneList)
+    rs= dbSendQuery(conn,paste0("SELECT  *  FROM GeneCoord WHERE value IN ('" , geneList , "');"))
+    location.df = fetch(rs, n=-1)
+    return (location.df)
+    
+  }
+  
+  
+  
+  getCircosData<-reactive({
+    data <- getLocation()
+    return(data)
+  })
+  
+  
+  output$locationCircos <- renderPlot({
+
+    #print(data)
+    chr.n  <- 7;
+    chrN     <- paste0("Chr", 1:5);
+    chrEN     <- c("ChrC", "ChrM");
+    chr = c(chrN, chrEN)
+    val     <- c(10, 11,20,15,20,0.05,0.1) ;
+    val2     <- c(15,15,15,15,14) ;
+    seg.dat <- data.frame(chr=chr, start=rep(1, chr.n), end=c(34.964571,22.037565,25.499034,20.862711,31.270811,0.154478,0.366924), name=chr, value=val);
+    #seg.dat2 <- data.frame(chr=chrN, start=rep(10, 5), end=c(11,11,11,11,11), name=chrN, value=val2);
+
+    seg.dat2 <- getCircosData()
+    
+    seg.c   <- segAnglePo(seg.dat, seg=chr);
+    
+    cols    <- "black";
+    
+    par(mar=c(2, 2, 2, 2))
+    plot(c(1,800), c(1,800), type="n", axes=FALSE, xlab="", ylab="");
+    circos(R=300,type="chr", cir=seg.c,  W=80 , print.chr.lab=TRUE, cex = 10)
+    circos(R=250, cir=seg.c, W=40, mapping=seg.dat2, type="b", col.v=5, col="black", B=T, cex=abs(seg.dat[,5])*1.5);
+    circos(R=220, cir=seg.c, W=40, mapping=seg.dat2, type="label2", col.v=5, col=cols, B=T, cex=0.8);
+
+  })
+
+
+
 
  ######################################################################
   
